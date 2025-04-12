67b6ef6096c1c931be85c1b647f12f2f570a5bc0
Samurai-code-gif <wildler.net@gmail.com>
2025-04-12 19:45:21 +0000

    Testesupertrunfo.changes

--- Testesupertrunfo.c
+++ Testesupertrunfo.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 
 int main() {
+    char estado1 = 'A';
+    char carta [5] = "A01";
+    char cidade1[20] = "Belem";
+    int populacao1 = 1398531;
+    float area1 = 1.0859;
+    float pib = 33.000000000
+    int pontos_turisticos = 50;
 
-    int  carta;
-    char estado;
-    int populacao;
-    flot area;
-    int pontos_turisticos;
+    printf("estado: %c\n", estdo1);
+    printf("codigo: %s\n", carta1);
+    printf("cidade: %s\n", cidade1);
+    printf("populacao:" %d\n, populacao1);
+    printf("area %2.f km^2\n", area1);
+    printf("pontos turisticos: %d\n", pontos_turisticos);
+    printf("\n");//comando de pular linha
+    printf("\n");//comando de pular linha
 
-    retunr 0;
+    char estado2 = "A";
+    char carta2  = "A02";
+    char cidade[20] = "santarem";
+    int populacao   = "331942";
+    float area2 = 22.887;
+    float pib2 = 6.390528;
+    int pontos_turisticos = 30;
+
+    printf("estado; %c\n", estado2);
+    printf("codigo: %s\n", carta2);
+    printf("cidade: %s\n", cidade);
+    printf("populacao: %d\n", populacao);
+    printf("area: %2.f km^2\n", area2);
+    printf("pib: %2.f\n", pib2);
+    printf("pontos tiristicos: %d\n", pontos_turisticos);
+
+
+
+
+    return 0;
 
 }
